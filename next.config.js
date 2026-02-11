ca3d712d3c074d78b9f2a9b7457236a59fa6667f
Lawrence Torres <torres.lawrence.986@gmail.com>
2026-02-12 04:44:24 +0800

    Update Next.js configuration to set basePath for consistent routing
    - Added basePath to nextConfig to ensure proper routing for the portfolio deployment on GitHub Pages.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export', // <=== enables static exports
   assetPrefix: '/nextjs-portfolio/', // <=== for assets like images, if needed
+  basePath: "/nextjs-portfolio",
   images: {
     unoptimized: true, // <=== disable Next.js image optimization (not compatible with static export)
   },
