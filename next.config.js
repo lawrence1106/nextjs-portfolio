612a1fcbe9d42b85117bb47deaab9a73094e75aa
Lawrence Torres <torres.lawrence.986@gmail.com>
2026-02-12 04:52:45 +0800

    Refactor Next.js configuration to support environment-specific asset handling
    - Updated nextConfig to use a function that determines assetPrefix based on the development phase, ensuring proper asset loading during development and production.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // <=== enables static exports
-  assetPrefix: '/nextjs-portfolio/', // <=== for assets like images, if needed
-  basePath: "/nextjs-portfolio",
-  images: {
-    unoptimized: true, // <=== disable Next.js image optimization (not compatible with static export)
-  },
+const nextConfig = phase => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+
+  return {
+    output: 'export', // <=== enables static exports
+    assetPrefix: isDev ? undefined : '/nextjs-portfolio',
+    basePath: "/nextjs-portfolio",
+    images: {
+      unoptimized: true, // <=== disable Next.js image optimization (not compatible with static export)
+    },
+  }
 };
 
 module.exports = nextConfig;
